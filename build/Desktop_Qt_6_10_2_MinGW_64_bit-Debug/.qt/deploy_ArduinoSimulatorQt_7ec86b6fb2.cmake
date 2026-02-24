1028954fc9dd5f3738f091c3fa56f2cd4d467cc9
ronakdevani <ronakdevani6606@gmail.com>
2026-02-24 16:49:23 +0100

    change setting to makefile and modify git file

--- build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/deploy_ArduinoSimulatorQt_7ec86b6fb2.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/Dell/Arduino-Simulator-Qt6/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ArduinoSimulatorQt-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/Dell/Arduino-Simulator-Qt6/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/ArduinoSimulatorQt.exe"
-    GENERATE_QT_CONF
-)
